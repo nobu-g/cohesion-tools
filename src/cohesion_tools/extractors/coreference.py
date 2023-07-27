e2aa17b7ad928f7c758e53e38b698a9e0a1f35e6
nobu-g <ueda-n127@hotmail.co.jp>
2023-07-27 17:26:11 +0900

    fix a bug

--- src/cohesion_tools/extractors/coreference.py
+++ src/cohesion_tools/extractors/coreference.py
@@ -10,7 +10,7 @@ class CoreferenceExtractor(BaseExtractor):
     def __init__(self, exophora_referent_types: List[ExophoraReferentType]) -> None:
         super().__init__(exophora_referent_types)
 
-    def extract(self, mention: BasePhrase) -> list:
+    def extract_rels(self, mention: BasePhrase) -> List[Union[BasePhrase, ExophoraReferent]]:
         referents: List[Union[BasePhrase, ExophoraReferent]] = []
         candidates: List[BasePhrase] = self.get_candidates(mention)
         for coreferent in mention.get_coreferents(include_nonidentical=False, include_self=False):
