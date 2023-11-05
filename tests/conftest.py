0ec1cd90e2e6ea2c3515169c92dd36e88d7183fd
nobu-g <ueda-n127@hotmail.co.jp>
2023-11-05 21:33:59 +0900

    update ruff and reformat code

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,22 +8,22 @@ from rhoknp.cohesion import ExophoraReferentType
 from cohesion_tools.evaluation import CohesionScorer
 
 
-@pytest.fixture
+@pytest.fixture()
 def data_dir() -> Path:
     return Path(__file__).parent / "data"
 
 
-@pytest.fixture
+@pytest.fixture()
 def predicted_documents(data_dir: Path) -> List[Document]:
     return [Document.from_knp(path.read_text()) for path in sorted(data_dir.glob("system/*.knp"))]
 
 
-@pytest.fixture
+@pytest.fixture()
 def gold_documents(data_dir: Path) -> List[Document]:
     return [Document.from_knp(path.read_text()) for path in sorted(data_dir.glob("gold/*.knp"))]
 
 
-@pytest.fixture
+@pytest.fixture()
 def scorer(data_dir: Path) -> CohesionScorer:
     return CohesionScorer(
         exophora_referent_types=list(map(ExophoraReferentType, ("著者", "読者", "不特定:人", "不特定:物"))),
@@ -35,11 +35,11 @@ def scorer(data_dir: Path) -> CohesionScorer:
     )
 
 
-@pytest.fixture
+@pytest.fixture()
 def abbreviated_documents(data_dir: Path) -> List[Document]:
     return [Document.from_knp(path.read_text()) for path in sorted(data_dir.glob("knp/*.knp"))]
 
 
-@pytest.fixture
+@pytest.fixture()
 def restored_documents(data_dir: Path) -> List[Document]:
     return [Document.from_knp(path.read_text()) for path in sorted(data_dir.glob("expected/restored/*.knp"))]
